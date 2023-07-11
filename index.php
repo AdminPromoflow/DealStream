afa44e9f8c18b3947e4339947666b53c50abe059
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-11 05:51:42 -0500

    Update index.php
    ...

--- index.php
+++ index.php
@@ -7,13 +7,27 @@
   <body>
     <style media="screen">
       body{
+        position: relative;
         background-image: url("Home/Img/bg1.jpg"); /* The image used */
         height: 100vh; /* You must set a specified height */
         background-position: center; /* Center the image */
         background-repeat: no-repeat; /* Do not repeat the image */
         background-size: cover; /* Resize the background image to cover the entire container */
       }
+      .bgSquare{
+        position: relative;
+        height: 90%;
+        width: 80%;
+        background: white;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+
+
+      }
     </style>
-    <h1>Hola</h1>
+    <div class="bgSquare">
+
+    </div>
   </body>
 </html>
